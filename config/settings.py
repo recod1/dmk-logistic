d4954409eb5e2691972a3da1d0751581e32b1f4f
Igor Konnov <igor.konnov.00@mail.ru>
2026-03-10 17:34:31 +0300

    Remove tokens

--- config/settings.py
+++ config/settings.py
@@ -40,10 +40,8 @@ class PointStatus(str, Enum):
 @dataclass
 class Settings:
     TG_TOKEN: str = os.getenv("TG_TOKEN", "")
-    # TG_TOKEN: str = os.getenv("TG_TOKEN", "7789436327:AAE7f-g-8HY-5UYayA8y5Y7lnh6NwzDgaOA")
     DB_PATH: str = os.getenv("DB_PATH", "./db/olymp.db")
     WIALON_TOKEN: str = os.getenv("WIALON_TOKEN", "")
-    # WIALON_TOKEN: str = os.getenv("WIALON_TOKEN", "1b78c9f1f54d396c5712d7620b06ad61C3C50B6594F0117363B7EA3F9B64F53A1722A315")
     WIALON_BASE_URL: str = os.getenv("WIALON_BASE_URL", "http://w1.wialon.justgps.ru")
     
     class Config:
